7f3a21ed083e3dee49ac838c18df89bca82a62c8
Ryan Huffman <ryanhuffman@gmail.com>
2016-12-12 11:16:47 -0800

    Fix models not settings textures if not loaded

--- libraries/render-utils/src/Model.cpp
+++ libraries/render-utils/src/Model.cpp
@@ -848,6 +848,10 @@ void Model::setTextures(const QVariantMap& textures) {
         _needsUpdateTextures = true;
         _needsFixupInScene = true;
         _renderGeometry->setTextures(textures);
+    } else {
+        connect(&_renderWatcher, &GeometryResourceWatcher::finished, this, [this, textures]() {
+            _renderGeometry->setTextures(textures);
+        });
     }
 }
 
