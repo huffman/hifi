5930f160a5b0638cbc49ce8af140915ff29f4f05
Delamare2112 <trevor@studiolimitless.com>
2017-03-14 13:20:58 -0700

    Moved to an Entity Client script

--- unpublishedScripts/interaction/Interaction.js
+++ unpublishedScripts/interaction/Interaction.js
@@ -1,106 +1,128 @@
 // Limitelss NPC interaction
-print("loading interaction script");
 
-var NPC = false;
-var hasCenteredOnNPC = false;
-var distance = 10;
-var r = 8;
+(function(){
+	print("loading interaction script");
 
-var baselineX = 0;
-var baselineY = 0;
-var nodRange = 20;
-var shakeRange = 20;
+	var Avatar = false;
+	var NPC = false;
+	var hasCenteredOnNPC = false;
+	var distance = 10;
+	var r = 8;
 
-MyAvatar.setListeningToVoice(false);
-
-MyAvatar.onFinishedSpeaking.connect(function(speech) {
-	print("Got: " + speech);
-	callOnNPC("voiceData: " + speech);
-});
-
-function setBaselineRotations(rot) {
-	baselineX = rot.x;
-	baselineY = rot.y;
-}
-
-function findLookedAtNPC() {
-	var intersection = AvatarList.findRayIntersection({origin: MyAvatar.position, direction: Quat.getFront(Camera.getOrientation())}, true);
-	if(intersection.intersects && intersection.distance <= distance){
-		var npcAvatar = AvatarList.getAvatar(intersection.avatarID);
-		if(npcAvatar.displayName.search("NPC") != -1) {
-			print("Found NPC!");
-			setBaselineRotations(Quat.safeEulerAngles(Camera.getOrientation()));
-			return intersection.avatarID;
-		}
-	}
-	return false;
-}
-
-function callOnNPC(message) {
-	Messages.sendMessage("interactionComs", message + ": " + NPC);
-}
-
-function isStillFocusedNPC() {
-	var avatar = AvatarList.getAvatar(NPC);
-	if(avatar) {
-		var avatarPosition = avatar.position;
-		return Vec3.distance(MyAvatar.position, avatarPosition) <= distance && Math.abs(Quat.dot(Camera.getOrientation(), Quat.lookAtSimple(MyAvatar.position, avatarPosition))) > 0.6;
-	}
-	return false; // NPC reference died. Maybe it crashed or we teleported to a new world?
-}
-
-function onWeLostFocus() {
-	print("lost NPC: " + NPC);
-	callOnNPC("onLostFocused");
 	var baselineX = 0;
 	var baselineY = 0;
-	MyAvatar.setListeningToVoice(false);
-}
+	var nodRange = 20;
+	var shakeRange = 20;
 
-function onWeGainedFocus() {
-	print("found NPC: " + NPC);
-	callOnNPC("onFocused");
-	var rotation = Quat.safeEulerAngles(Camera.getOrientation());
-	baselineX = rotation.x;
-	baselineY = rotation.y;
-	MyAvatar.setListeningToVoice(true);
-}
+	var ticker = false;
 
-function checkFocus() {
-	var newNPC = findLookedAtNPC();
-
-	if(NPC && newNPC != NPC && !isStillFocusedNPC()) {
-		onWeLostFocus();
-		NPC = false;
+	function callOnNPC(message) {
+		Messages.sendMessage("interactionComs", message + ": " + NPC);
 	}
-	if(!NPC && newNPC != false) {
-		NPC = newNPC;
-		onWeGainedFocus();
+
+	MyAvatar.onFinishedSpeaking.connect(function(speech) {
+		print("Got: " + speech);
+		callOnNPC("voiceData: " + speech);
+	});
+
+	function setBaselineRotations(rot) {
+		baselineX = rot.x;
+		baselineY = rot.y;
 	}
-}
 
-function checkGesture() {
-	var rotation = Quat.safeEulerAngles(Camera.getOrientation());
+	function findLookedAtNPC() {
+		var intersection = AvatarList.findRayIntersection({origin: MyAvatar.position, direction: Quat.getFront(Camera.getOrientation())}, true);
+		if(intersection.intersects && intersection.distance <= distance){
+			var npcAvatar = AvatarList.getAvatar(intersection.avatarID);
+			if(npcAvatar.displayName.search("NPC") != -1) {
+				print("Found NPC!");
+				setBaselineRotations(Quat.safeEulerAngles(Camera.getOrientation()));
+				return intersection.avatarID;
+			}
+		}
+		return false;
+	}
 
-	var deltaX = Math.abs(rotation.x - baselineX);
-	if(deltaX > 180)
-		deltaX -= 180;
-	var deltaY = Math.abs(rotation.y - baselineY);
-	if(deltaY > 180)
-		deltaY -= 180;
+	function isStillFocusedNPC() {
+		var avatar = AvatarList.getAvatar(NPC);
+		if(avatar) {
+			var avatarPosition = avatar.position;
+			return Vec3.distance(MyAvatar.position, avatarPosition) <= distance && Math.abs(Quat.dot(Camera.getOrientation(), Quat.lookAtSimple(MyAvatar.position, avatarPosition))) > 0.6;
+		}
+		return false; // NPC reference died. Maybe it crashed or we teleported to a new world?
+	}
 
-	if(deltaX >= nodRange && deltaY <= shakeRange)
-		callOnNPC("onNodReceived");
-	else if (deltaY >= shakeRange && deltaX <= nodRange)
-		callOnNPC("onShakeReceived");
-}
+	function onWeLostFocus() {
+		print("lost NPC: " + NPC);
+		callOnNPC("onLostFocused");
+		var baselineX = 0;
+		var baselineY = 0;
+		MyAvatar.setListeningToVoice(false);
+	}
 
-function tick () {
-	checkFocus();
-	if(NPC)
-		checkGesture();
-}
+	function onWeGainedFocus() {
+		print("found NPC: " + NPC);
+		callOnNPC("onFocused");
+		var rotation = Quat.safeEulerAngles(Camera.getOrientation());
+		baselineX = rotation.x;
+		baselineY = rotation.y;
+		MyAvatar.setListeningToVoice(true);
+	}
 
-Script.update.connect(tick);
+	function checkFocus() {
+		var newNPC = findLookedAtNPC();
 
-print("finished loading interaction script");
+		if(NPC && newNPC != NPC && !isStillFocusedNPC()) {
+			onWeLostFocus();
+			NPC = false;
+		}
+		if(!NPC && newNPC != false) {
+			NPC = newNPC;
+			onWeGainedFocus();
+		}
+	}
+
+	function checkGesture() {
+		var rotation = Quat.safeEulerAngles(Camera.getOrientation());
+
+		var deltaX = Math.abs(rotation.x - baselineX);
+		if(deltaX > 180)
+			deltaX -= 180;
+		var deltaY = Math.abs(rotation.y - baselineY);
+		if(deltaY > 180)
+			deltaY -= 180;
+
+		if(deltaX >= nodRange && deltaY <= shakeRange)
+			callOnNPC("onNodReceived");
+		else if (deltaY >= shakeRange && deltaX <= nodRange)
+			callOnNPC("onShakeReceived");
+	}
+
+	function tick() {
+		checkFocus();
+		if(NPC)
+			checkGesture();
+	}
+
+	this.enterEntity = function(id) {
+		print("Something entered me: " + id);
+		if(!ticker) {
+			ticker = Script.setInterval(tick, 333);
+		}
+	};
+	this.leaveEntity = function(id) {
+		print("Something left me: " + id);
+		if(ticker) {
+			ticker.stop();
+			ticker = false;
+		}
+	};
+	this.unload = function() {
+		print("Okay. I'm Unloading!");
+		if(ticker) {
+			ticker.stop();
+			ticker = false;
+		}
+	}
+	print("finished loading interaction script");
+})
