cf557c2c8df604d48c41c965e42a3bb46fbacea9
Ryan Huffman <ryanhuffman@gmail.com>
2019-07-24 11:20:08 -0700

    Fix qmlToScript getting called on script thread and causing crash

--- interface/src/ui/InteractiveWindow.cpp
+++ interface/src/ui/InteractiveWindow.cpp
@@ -203,7 +203,7 @@ InteractiveWindow::InteractiveWindow(const QString& sourceUrl, const QVariantMap
                 QQuickItem* rootItem = _dockWidget->getRootItem();
                 _dockWidget->getQuickView()->rootContext()->setContextProperty(EVENT_BRIDGE_PROPERTY, _interactiveWindowProxy);
                 QObject::connect(rootItem, SIGNAL(sendToScript(QVariant)), this, SLOT(qmlToScript(const QVariant&)),
-                                 Qt::QueuedConnection);
+                                 Qt::DirectConnection);
                 QObject::connect(rootItem, SIGNAL(keyPressEvent(int, int)), this, SLOT(forwardKeyPressEvent(int, int)),
                                  Qt::QueuedConnection);
                 QObject::connect(rootItem, SIGNAL(keyReleaseEvent(int, int)), this, SLOT(forwardKeyReleaseEvent(int, int)),
@@ -244,7 +244,7 @@ InteractiveWindow::InteractiveWindow(const QString& sourceUrl, const QVariantMap
                 object->setProperty(VISIBLE_PROPERTY, properties[INTERACTIVE_WINDOW_VISIBLE_PROPERTY].toBool());
             }
 
-            connect(object, SIGNAL(sendToScript(QVariant)), this, SLOT(qmlToScript(const QVariant&)), Qt::QueuedConnection);
+            connect(object, SIGNAL(sendToScript(QVariant)), this, SLOT(qmlToScript(const QVariant&)), Qt::DirectConnection);
             QObject::connect(object, SIGNAL(keyPressEvent(int, int)), this, SLOT(forwardKeyPressEvent(int, int)),
                              Qt::QueuedConnection);
             QObject::connect(object, SIGNAL(keyReleaseEvent(int, int)), this, SLOT(forwardKeyReleaseEvent(int, int)),
@@ -336,13 +336,21 @@ void InteractiveWindow::raise() {
     }
 }
 
-void InteractiveWindow::qmlToScript(const QVariant& message) {
+void InteractiveWindow::qmlToScript(const QVariant& originalMessage) {
+    QVariant message = originalMessage;
     if (message.canConvert<QJSValue>()) {
-        emit fromQml(qvariant_cast<QJSValue>(message).toVariant());
+        message = qvariant_cast<QJSValue>(message).toVariant();
     } else if (message.canConvert<QString>()) {
-        emit fromQml(message.toString());
+        message = message.toString();
     } else {
         qWarning() << "Unsupported message type " << message;
+        return;
+    }
+
+    if (thread() != QThread::currentThread()) {
+        QMetaObject::invokeMethod(this, "fromQml", Q_ARG(const QVariant&, message));
+    } else {
+        emit fromQml(message);
     }
 }
 
